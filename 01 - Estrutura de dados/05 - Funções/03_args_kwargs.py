ce008a8c3a74911bfe6676f7ab380bfc0de70324
rflombardi <rflombardi36@gmail.com>
2024-05-16 17:48:15 -0300

    Adicionado data por extenso ao poema

--- 01 - Estrutura de dados/05 - Funções/03_args_kwargs.py
+++ 01 - Estrutura de dados/05 - Funções/03_args_kwargs.py
@@ -1,11 +1,13 @@
 def exibir_poema(data_extenso, *args, **kwargs):
     texto = "\n".join(args)
-    meta_dados = "\n".join([f"{chave.title()}: {valor}" for chave, valor in kwargs.items()])
+    meta_dados = "\n".join(
+        [f"{chave.title()}: {valor}" for chave, valor in kwargs.items()])
     mensagem = f"{data_extenso}\n\n{texto}\n\n{meta_dados}"
     print(mensagem)
 
 
 exibir_poema(
+    "Sexta-feira, 16 de Maio de 2024",
     "Zen of Python",
     "Beautiful is better than ugly.",
     "Explicit is better than implicit.",
